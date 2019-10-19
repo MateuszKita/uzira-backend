ea78a8f826f10b84558fa738d41c14e4f175b37b
MateuszKita <mkita.zg@gmail.com>
2019-10-19 12:13:21 +0200

    change mongodb database urls

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -4,18 +4,39 @@ export const connectToMongo = async () => {
     const mongoUrl = process.env.MONGODB_URI;
 
     console.log('mongoUrl', mongoUrl);
+    //
+    // if (mongoUrl) {
+    //     try {
+    //         await connect(mongoUrl, {
+    //             useNewUrlParser: true,
+    //             useCreateIndex: true,
+    //             useFindAndModify: false
+    //         });
+    //     } catch (e) {
+    //         console.log('Could not connect to MongoDB', e);
+    //     }
+    // } else {
+    //     console.error('Could not read MongoDB URL');
+    // }
 
-    if (mongoUrl) {
-        try {
-            await connect(mongoUrl, {
-                useNewUrlParser: true,
-                useCreateIndex: true,
-                useFindAndModify: false
-            });
-        } catch (e) {
-            console.log('Could not connect to MongoDB', e);
-        }
-    } else {
-        console.error('Could not read MongoDB URL');
-    }
+    const options = {
+        autoIndex: false, // Don't build indexes
+        reconnectTries: 30, // Retry up to 30 times
+        reconnectInterval: 500, // Reconnect every 500ms
+        poolSize: 10, // Maintain up to 10 socket connections
+        // If not connected, return errors immediately rather than waiting for reconnect
+        bufferMaxEntries: 0
+    };
+
+    const connectWithRetry = async () => {
+        console.log('MongoDB connection with retry');
+        await connect('mongodb://mongo:27017/test', options).then(() => {
+            console.log('MongoDB is connected');
+        }).catch(() => {
+            console.log('MongoDB connection unsuccessful, retry after 5 seconds.');
+            setTimeout(connectWithRetry, 5000);
+        });
+    };
+
+    await connectWithRetry();
 };
