e7bf9eb876d36738bd6ae1a1a0189db2f11a2552
MateuszKita <mkita.zg@gmail.com>
2019-10-20 13:26:49 +0200

    change mongodb database urls

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -3,43 +3,35 @@ import {connect} from 'mongoose';
 export const connectToMongo = async () => {
     const mongoUrl = process.env.MONGODB_URI;
 
-    console.log('mongoUrl', mongoUrl);
-    //
-    // if (mongoUrl) {
-    //     try {
-    //         await connect(mongoUrl, {
-    //             useNewUrlParser: true,
-    //             useCreateIndex: true,
-    //             useFindAndModify: false
-    //         });
-    //     } catch (e) {
-    //         console.log('Could not connect to MongoDB', e);
-    //     }
-    // } else {
-    //     console.error('Could not read MongoDB URL');
-    // }
+    if (mongoUrl) {
+        const maxRetryTimes = 10;
+        const timeToRetry = 10000;
 
-    const options = {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        // autoIndex: false, // Don't build indexes
-        // reconnectTries: 30, // Retry up to 30 times
-        // reconnectInterval: 500, // Reconnect every 500ms
-        // poolSize: 10, // Maintain up to 10 socket connections
-        // // If not connected, return errors immediately rather than waiting for reconnect
-        // bufferMaxEntries: 0
-    };
+        let connectionRetries = 0;
 
-    const connectWithRetry = async () => {
-        console.log('MongoDB connection with retry');
-        await connect(mongoUrl as string, options).then(() => {
-            console.log('MongoDB is connected');
-        }).catch((err) => {
-            console.log('MongoDB connection unsuccessful, retry after 5 seconds.', err);
-            setTimeout(connectWithRetry, 5000);
-        });
-    };
+        const options = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        };
 
-    await connectWithRetry();
+        const connectWithRetry = async () => {
+            console.log(connectionRetries === 0 ? 'Trying to connect to MongoDB' : 'Retrying to connect to MongoD');
+            await connect(mongoUrl, options).then(() => {
+                console.log('MongoDB is connected');
+            }).catch((err) => {
+                if (connectionRetries <= maxRetryTimes) {
+                    console.log(`MongoDB connection unsuccessful, retry after ${timeToRetry / 1000} seconds.`, err);
+                    connectionRetries++;
+                    setTimeout(connectWithRetry, timeToRetry);
+                } else {
+                    console.error('Could not establish connection to MongoDB after 10 retries, giving up...', err);
+                }
+            });
+        };
+
+        await connectWithRetry();
+    } else {
+        console.error('Could not read MongoDB URL');
+    }
 };
