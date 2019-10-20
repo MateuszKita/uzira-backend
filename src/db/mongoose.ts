a4e7688346bdddb166aa6dcc6e9ebf808557d3eb
MateuszKita <mkita.zg@gmail.com>
2019-10-20 13:10:27 +0200

    change mongodb database urls

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -23,12 +23,12 @@ export const connectToMongo = async () => {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
-        autoIndex: false, // Don't build indexes
-        reconnectTries: 30, // Retry up to 30 times
-        reconnectInterval: 500, // Reconnect every 500ms
-        poolSize: 10, // Maintain up to 10 socket connections
-        // If not connected, return errors immediately rather than waiting for reconnect
-        bufferMaxEntries: 0
+        // autoIndex: false, // Don't build indexes
+        // reconnectTries: 30, // Retry up to 30 times
+        // reconnectInterval: 500, // Reconnect every 500ms
+        // poolSize: 10, // Maintain up to 10 socket connections
+        // // If not connected, return errors immediately rather than waiting for reconnect
+        // bufferMaxEntries: 0
     };
 
     const connectWithRetry = async () => {
