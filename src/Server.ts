3454a4ca4e0a3aa03344fb8b179eb2c083a814db
MateuszKita <mkita.zg@gmail.com>
2019-11-03 23:13:51 +0100

    allow CORS v2

--- src/Server.ts
+++ src/Server.ts
@@ -9,7 +9,12 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+    );
     next();
 });
 app.use(logger('dev'));
