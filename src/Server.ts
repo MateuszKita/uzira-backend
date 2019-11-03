2267772eb8a6fffabfef927a3a1e3ecafe571dab
MateuszKita <mkita.zg@gmail.com>
2019-11-03 22:56:37 +0100

    allow CORS

--- src/Server.ts
+++ src/Server.ts
@@ -7,6 +7,11 @@ import {connectToMongo} from './db/mongoose';
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
