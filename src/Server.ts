4d79729281325f7deb9c8bcc2dd88ebb1ef1523e
MateuszKita <mkita.zg@gmail.com>
2019-11-04 21:55:46 +0100

    allow CORS v21

--- src/Server.ts
+++ src/Server.ts
@@ -6,6 +6,26 @@ import cookieParser = require('cookie-parser');
 
 const app = express();
 
+const allowedOrigins = ['https://uzira.netlify.com/', 'http://localhost:3000'];
+
+app.use(cors({
+    // origin: (origin, callback) => {
+    //     if (!origin) {
+    //         return callback(null, true);
+    //     }
+    //     if (allowedOrigins.indexOf(origin) === -1) {
+    //         const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+    //         return callback(new Error(msg), false);
+    //     }
+    //     return callback(null, true);
+    // },
+    origin: '*',
+    exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar', 'Access-Control-Allow-Origin'],
+    credentials: true,
+    methods: 'GET, PUT, POST, DELETE, PATCH, OPTIONS',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+}));
+
 app.use(cookieParser());
 
 app.use('', BaseRouter);
