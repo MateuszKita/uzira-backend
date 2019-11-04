e766ae094c0640a7f1767857fc1ccd0637f693ac
MateuszKita <mkita.zg@gmail.com>
2019-11-04 20:51:11 +0100

    allow CORS v14

--- src/Server.ts
+++ src/Server.ts
@@ -8,20 +8,16 @@ import {NO_CONTENT} from 'http-status-codes';
 const app = express();
 
 app.options('*', cors());
+
 app.use(cors({
     origin: '*',
     methods: 'OPTIONS, GET, HEAD, PUT, PATCH, POST, DELETE',
-    preflightContinue: false,
+    preflightContinue: true,
     credentials: true,
     optionsSuccessStatus: NO_CONTENT,
     allowedHeaders: 'X-Requested-With,content-type'
 }));
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use('', BaseRouter);
 
 connectToMongo().then(() => {
