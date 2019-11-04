072e593ce074d2e2f98b1d6b3162816c63d1d144
MateuszKita <mkita.zg@gmail.com>
2019-11-04 20:17:34 +0100

    allow CORS v10

--- src/Server.ts
+++ src/Server.ts
@@ -9,11 +9,11 @@ import {OK} from 'http-status-codes';
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use('', BaseRouter);
 
 app.use(cors({
