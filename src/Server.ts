40c00514618bb96be7c086e6d3a7dfc6d5d82127
MateuszKita <mkita.zg@gmail.com>
2019-11-04 18:10:51 +0100

    allow CORS v4

--- src/Server.ts
+++ src/Server.ts
@@ -9,11 +9,8 @@ const app = express();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Origin', 'https://uzira.netlify.com/');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
 app.use(logger('dev'));
