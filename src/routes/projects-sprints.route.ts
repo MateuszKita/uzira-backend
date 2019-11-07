b3f1908047be989cebee17de74bd8bf019f3c8de
MateuszKita <mkita.zg@gmail.com>
2019-11-07 13:43:56 +0100

    change projects structure part 2

--- src/routes/projects-sprints.route.ts
+++ src/routes/projects-sprints.route.ts
@@ -1,8 +1,5 @@
 import {Request, Response, Router} from 'express';
 import {BAD_REQUEST, NOT_FOUND} from 'http-status-codes';
-import {Task} from '../mongoose/tasks.mongoose';
-import {ITask} from '../models/tasks.model';
-import {IProjectBacklog} from '../models/projects.model';
 import {Project} from '../mongoose/projects.mongoose';
 import {IAuthorizedRequest, IUser} from '../models/users.model';
 import {auth} from '../middleware/authorization';
@@ -57,65 +54,6 @@ router.post('/:id/sprints', auth, async (req: Request, res: Response) => {
             await project.save();
             res.send({
                 message: `Updated project \'${project.toObject().name}\' by adding sprint: \'Sprint ${sprintIndex}\'`
-            });
-        } else {
-            res.status(NOT_FOUND).send();
-        }
-    } catch (e) {
-        console.error(e);
-        res.status(BAD_REQUEST).send(e);
-    }
-});
-
-/******************************************************************************
- *                      Get project backlog / Specific User - "GET /projects/:id/backlog"
- ******************************************************************************/
-
-router.get('/:id/backlog', auth, async (req: Request, res: Response) => {
-    try {
-        const projectId = req.params.id;
-        const user = (req as any as IAuthorizedRequest).user;
-        const project = await Project.findOne({_id: projectId, users: {$elemMatch: {_id: user._id}}});
-        if (project) {
-            const projectBacklog = project.toObject().backlog;
-            res.send(projectBacklog);
-        } else {
-            res.status(NOT_FOUND).send();
-        }
-    } catch (e) {
-        console.error(e);
-        res.status(BAD_REQUEST).send(e);
-    }
-});
-
-/******************************************************************************
- *                      Add task to project backlog / Specific User - "POST /projects/:id/backlog/"
- ******************************************************************************/
-
-router.post('/:id/backlog', auth, async (req: Request, res: Response) => {
-    try {
-        const projectId = req.params.id;
-        const user = (req as any as IAuthorizedRequest).user;
-        const project = await Project.findOne({_id: projectId, users: {$elemMatch: {_id: user._id}}});
-        if (project) {
-            const projectBacklog: IProjectBacklog = project.toObject().backlog;
-            const taskObject: ITask = req.body;
-            taskObject.projectId = projectId;
-            const task = new Task(taskObject);
-            await task.save();
-            const taskWithId: ITask = (await Task.findOne(taskObject) as any).toObject();
-            await project.update({
-                ...project.toObject(),
-                backlog: {
-                    tasks: [
-                        ...projectBacklog.tasks,
-                        taskWithId
-                    ]
-                }
-            });
-            await project.save();
-            res.send({
-                message: `Updated project \'${project.toObject().name}\' backlog by adding task: \'${taskObject.name}\'`
             });
         } else {
             res.status(NOT_FOUND).send();
--- src/routes/projects.route.ts
+++ src/routes/projects.route.ts
@@ -4,7 +4,6 @@ import {IAuthorizedRequest, IUser} from '../models/users.model';
 import {auth} from '../middleware/authorization';
 import {Project} from '../mongoose/projects.mongoose';
 import {User} from '../mongoose/users.mongoose';
-import {ISprint} from '../models/sprints.model';
 import {IProjectBacklog} from '../models/projects.model';
 import {Task} from '../mongoose/tasks.mongoose';
 import {ITask} from '../models/tasks.model';
@@ -156,6 +155,65 @@ router.post('/:id/users/:userId', auth, async (req: Request, res: Response) => {
             await project.save();
             res.send({
                 message: `Removed user from project \'${project.toObject().name}\'`
+            });
+        } else {
+            res.status(NOT_FOUND).send();
+        }
+    } catch (e) {
+        console.error(e);
+        res.status(BAD_REQUEST).send(e);
+    }
+});
+
+/******************************************************************************
+ *                      Get project backlog / Specific User - "GET /projects/:id/backlog"
+ ******************************************************************************/
+
+router.get('/:id/backlog', auth, async (req: Request, res: Response) => {
+    try {
+        const projectId = req.params.id;
+        const user = (req as any as IAuthorizedRequest).user;
+        const project = await Project.findOne({_id: projectId, users: {$elemMatch: {_id: user._id}}});
+        if (project) {
+            const projectBacklog = project.toObject().backlog;
+            res.send(projectBacklog);
+        } else {
+            res.status(NOT_FOUND).send();
+        }
+    } catch (e) {
+        console.error(e);
+        res.status(BAD_REQUEST).send(e);
+    }
+});
+
+/******************************************************************************
+ *                      Add task to project backlog / Specific User - "POST /projects/:id/backlog/"
+ ******************************************************************************/
+
+router.post('/:id/backlog', auth, async (req: Request, res: Response) => {
+    try {
+        const projectId = req.params.id;
+        const user = (req as any as IAuthorizedRequest).user;
+        const project = await Project.findOne({_id: projectId, users: {$elemMatch: {_id: user._id}}});
+        if (project) {
+            const projectBacklog: IProjectBacklog = project.toObject().backlog;
+            const taskObject: ITask = req.body;
+            taskObject.projectId = projectId;
+            const task = new Task(taskObject);
+            await task.save();
+            const taskWithId: ITask = (await Task.findOne(taskObject) as any).toObject();
+            await project.update({
+                ...project.toObject(),
+                backlog: {
+                    tasks: [
+                        ...projectBacklog.tasks,
+                        taskWithId
+                    ]
+                }
+            });
+            await project.save();
+            res.send({
+                message: `Updated project \'${project.toObject().name}\' backlog by adding task: \'${taskObject.name}\'`
             });
         } else {
             res.status(NOT_FOUND).send();
